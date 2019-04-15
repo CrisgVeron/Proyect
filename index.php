dda3d7d70875a81f6a322adca90629b50280187b
Criss <krizu.veron@outlook.es>
2019-04-15 10:28:39 -0300

    XXXXX

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba de Git</title>
+	<link href="menu/estilo.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+<body>
+	<div>
+		<?php include('menu/menu.php');?>
+	</div>
+	Sitio de prueba 
+</body>
+</html>
