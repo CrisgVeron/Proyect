c331d39422d8efdb2d31dce55b6f6e809be2e408
Criss <krizu.veron@outlook.es>
2019-04-15 13:33:09 -0300

    cambios en el index

--- index.php
+++ index.php
@@ -3,12 +3,24 @@
 <head>
 	<title>Prueba de Git</title>
 	<link href="menu/estilo.css" rel="stylesheet" type="text/css" media="screen">
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
 </head>
 <body>
-	<div>
-		<?php include('menu/menu.php');?>
-	</div>
-	Sitio de prueba
+	<header>
+		<div>
+			<?php include('menu/menu.php');?>
+		</div>
+	</header>
+	 <h3>Sitio de prueba</h3>
+	 <div>
+	 	1.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	 	
+	 </div>
 	<script src="menu/menu.js"></script> 
 </body>
 </html>
