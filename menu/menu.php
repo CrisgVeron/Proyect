f62136c662bbc65f0a5b4f3e5397b2425a6768f0
Criss <krizu.veron@outlook.es>
2019-04-15 11:20:14 -0300

    cambio en el menu

--- menu/menu.php
+++ menu/menu.php
@@ -1,11 +1,11 @@
 <header>
-	<div class="contenedor">
+	<div class="contenedor" >
 		<div class="logo">
 			<img src="http://dummyimage.com/200x100/000/fff&text=LOGO" alt="">
 		</div>
 		<nav class="menu">
 			<ul>
-				<li><a href="#">Inicio</a></li>
+				<li><a href="index.php">Inicio</a></li>
 				<li><a href="#">Blog</a></li>
 				<li><a href="#">Proyectos</a></li>
 				<li><a href="#">Trabajos</a></li>
